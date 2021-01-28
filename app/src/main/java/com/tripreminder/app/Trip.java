f53c8470eab516e47b1dccf8307056432dad4968
asmaaghoname <asmaaghoname204@gmail.com>
2021-01-29 01:39:30 +0200

    Trip History View

---
+++ app/src/main/java/com/tripreminder/app/Trip.java
@@ -0,0 +1,5 @@
+package com.tripreminder.app;
+
+public class Trip {
+
+}
--- app/src/main/java/com/tripreminder/app/TripsHistory.java
+++ app/src/main/java/com/tripreminder/app/TripsHistory.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class TripsHistory extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.single_trip);
+        setContentView(R.layout.activity_trips_history);
     }
 }
