de8e8cda2578fc816ff1d02e2fb3dc3b3502680f
AbanobWadie <abanob.wadi3@gmail.com>
2021-01-29 02:24:16 +0200

    Upcoming Trip View ++
    update XML

--- app/src/main/java/com/tripreminder/app/UpcomingTrip.java
+++ app/src/main/java/com/tripreminder/app/UpcomingTrip.java
@@ -5,15 +5,18 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class UpcomingTrip extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    Button addBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upcoming_trip);
+        addBtn = findViewById(R.id.addBtn);
 
         Trip[] trips = new Trip[1];
         //test data
