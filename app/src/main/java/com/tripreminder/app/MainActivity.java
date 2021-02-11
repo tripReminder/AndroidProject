93adb91c940e5a470cbc1ecc872df833944324dc
asmaaghoname <asmaaghoname204@gmail.com>
2021-02-12 00:47:27 +0200

    Final

--- app/src/main/java/com/tripreminder/app/MainActivity.java
+++ app/src/main/java/com/tripreminder/app/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModelProviders;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -13,11 +14,21 @@ import android.widget.Toast;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+    private  static int SPLASH_TIME_OUT=4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+               Intent splash = new Intent(MainActivity.this,FirstPage.class);
+               startActivity(splash);
+               finish();
+            }
+        },SPLASH_TIME_OUT);
 
     }
 }
